62bfd2f78463a1d4afc2b265c809c241ff3a3d8b
om <omsingh8400@gmail.com>
2026-01-18 19:36:36 +0530

    created user in the fire base

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,22 +1,35 @@
-import { getDatabase, ref, set } from "firebase/database";
-import { app } from "/firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "./firebase";
 import "./App.css";
+import Signup from "./pages/Signup";
 
-const db = getDatabase(app);
+
+
+
+
+const auth = getAuth(app);
 
 function App() {
-  const putData = () => {
-    set(ref(db, "users/piyush"), {
-      id: 1,
-      name: "Piyush",
-      age: 21,
-    });
+  const signupUser = () => {
+    createUserWithEmailAndPassword(
+      auth,
+      "omsingh8400@gmail.com",
+      "om123456"
+    )
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
     <div className="App">
       <h1>Firebase React App</h1>
-      <button onClick={putData}>Put Data</button>
+      <button onClick={signupUser}>Create User</button>
+
+       <Signup />
     </div>
   );
 }
