507238e0d2051e2a7628eab18442ffdf9062fb6e
om <omsingh8400@gmail.com>
2026-01-19 17:33:09 +0530

    hashed the password using salt

--- backend project task manager/src/models/User.js
+++ backend project task manager/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,6 +27,13 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 export default User;
