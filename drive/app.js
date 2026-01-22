6d031b9ee9d3dd4a754116054988f3cb0642ff9f
om <omsingh8400@gmail.com>
2026-01-22 09:12:30 +0530

    ejs setup done

--- drive/app.js
+++ drive/app.js
@@ -3,8 +3,11 @@ const express = require('express');
  const  app = express();
 
 
+  app.set ('view engine', 'ejs');
+
+
   app.get('/',(req,res)=> {
-     res.send('hello');
+     res.render('index.ejs')
 
 
   })
