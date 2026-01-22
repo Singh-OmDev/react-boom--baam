3bc3af6c2d7e668b133128371e0a709e6748617e
om <omsingh8400@gmail.com>
2026-01-22 12:24:02 +0530

    ss

--- drive/app.js
+++ drive/app.js
@@ -6,6 +6,10 @@ const express = require('express');
 
   app.set ('view engine', 'ejs');
 
+   app.use(express.json());
+
+    app.use(express.urlencoded({extended:true}))
+
    app.use('/user', userRouter);
 
 
--- drive/routes/user.routes.js
+++ drive/routes/user.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const router = express.Router();
 
- const router  = express.Router();
+router.get('/register', (req, res) => {
+  res.render('register');
+});
 
-
- router.get('/register', (req,res)=> {
-
-     res.render('register');
- })
-  
+router.post('/register', (req, res) => {
+  console.log(req.body);
+  res.send("User registered");
+});
 
 module.exports = router;
