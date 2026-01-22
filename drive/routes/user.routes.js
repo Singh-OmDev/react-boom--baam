377d5839d1292dfc476c9d81a928c8863101ede8
om <omsingh8400@gmail.com>
2026-01-22 22:54:58 +0530

    jwt done

--- drive/routes/user.routes.js
+++ drive/routes/user.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
  const bcrypt  = require('bcrypt');
+ const jwt = require ('jsonwebtoken');
+
 
 const userModel = require('../models/user.model');
 const { body, validationResult } = require('express-validator');
@@ -61,5 +63,58 @@ router.post(
  router.get('/login', (req, res)=> {
      res.render('login')
  })
+
+ router.post ('/login',
+    body('username'.trim().isLength({min:3})),
+    body('password'.trim().isLength({min:5})),
+    async(req,res)=> {
+         const errors = validationResult(req);
+
+         if (!errors.isEmpty()){
+             return res.status(400).json({
+                error:errors.array(),
+                message:'invalid data'
+             })
+         }
+
+          const {username , password} = req.body;
+           const user = await userModel.findOne({
+
+             username:username
+           })
+
+            if (!user){
+
+                  return res.status (400).json({
+
+                     message:'username or password is incorrect'
+                  })
+            }
+
+            if (!isMatch){
+
+                 return res.status(400).json({
+                    message: 'username or password is incorrect'
+                 })
+            }
+
+
+            const  token = jwt.sign ({
+
+
+                 userId :user._id,
+                  email:user.email,
+                   username:user.username
+            },
+
+process.env.JWT_SECRET,
+
+
+        )
+
+
+
+    }
+ )
 
 module.exports = router;
