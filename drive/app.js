f624afac0247d3551835ef8fd15cf1365ff20717
om <omsingh8400@gmail.com>
2026-01-22 09:21:45 +0530

    routes created

--- drive/app.js
+++ drive/app.js
@@ -2,15 +2,12 @@ const express = require('express');
 
  const  app = express();
 
+ const userRouter = require ('./routes/user.routes');
 
   app.set ('view engine', 'ejs');
 
+   app.use('/user', userRouter);
 
-  app.get('/',(req,res)=> {
-     res.render('index.ejs')
-
-
-  })
 
    app.listen(3000,()=> {
      console.log('server is running on port 3000');
