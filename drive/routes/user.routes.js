ff91b99fd19b81d19a8818f4e0c1080dfaf8e90d
om <omsingh8400@gmail.com>
2026-01-22 11:54:48 +0530

    form created

--- drive/routes/user.routes.js
+++ drive/routes/user.routes.js
@@ -3,10 +3,10 @@ const express = require('express');
  const router  = express.Router();
 
 
-  router.get('/test', (req,res)=> {
-       res.send ('user test route');
+ router.get('/register', (req,res)=> {
 
-
-  })
+     res.render('register');
+ })
+  
 
 module.exports = router;
