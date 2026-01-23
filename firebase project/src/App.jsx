4c58ce5e00e648ee8f6be93f59ef0a5d391e0386
om <omsingh8400@gmail.com>
2026-01-23 15:03:01 +0530

    register page done

--- firebase project/src/App.jsx
+++ firebase project/src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import Register from './pages/Register'
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<h1>home</h1>} />
         <Route path='/login' element={<h1>login</h1>} />
+
+         <Route path='/register' element={<Register />}  />
       </Routes>
     </div>
   )
