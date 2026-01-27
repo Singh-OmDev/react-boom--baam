f9830aa269932845c7a037b79db591b27e9f51df
om <omsingh8400@gmail.com>
2026-01-27 18:08:43 +0530

    fixed

--- redux/src/components/SearchBar.jsx
+++ redux/src/components/SearchBar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchBar = () => {
+
+const  [text, setText] = useState ();
+
+
+
+ const submitHandler = (e)=> {
+   e.preventDefault()
+    
+ }
+
+
   return (
     <div>
-    <form>
-       <input type='text' placeholder='search anything ....'></input>
+    <form  onSubmit={(e)=> {
+       submitHandler(e)
+    }} className='flex  bg-gray-900 py-10 px-14'>
+       <input 
+        value={text}
+         onChange={(e)=> {
+          setText(e.target.value);
+          
+         }}
+        className='w-full border-2 px-6 py-3 text-xl rounded outline-none'
+         type='text' placeholder='search anything ....'></input>
 
         <button> search </button>
     </form>
