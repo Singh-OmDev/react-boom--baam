c0668d7f4798b6a5c959a142fbaa348adcabc3df
om <omsingh8400@gmail.com>
2026-01-27 18:18:22 +0530

    added provider

--- redux/src/main.jsx
+++ redux/src/main.jsx
@@ -6,7 +6,12 @@ import { store } from './redux/store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-      <App />
+
+ <Provider store={store} >
+
+        <App />
+ </Provider>
+    
        
   
   </StrictMode>,
