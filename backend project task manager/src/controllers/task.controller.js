67640647e88b40ed5aad729b8579b599ae9b11ca
om <omsingh8400@gmail.com>
2026-02-10 22:15:42 +0530

    created update and delete route

--- backend project task manager/src/controllers/task.controller.js
+++ backend project task manager/src/controllers/task.controller.js
@@ -1,7 +1,73 @@
 import Task from "../models/Task.js";
 
 
+
+//delete task
+
+export const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      user: req.userId, // 🔥 ownership check again
+    });
+
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task not found or unauthorized",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Task deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+
  
+//update task 
+
+export const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      user: req.userId, // 🔥 ownership check
+    });
+
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task not found or unauthorized",
+      });
+    }
+
+    task.title = req.body.title ?? task.title;
+    task.description = req.body.description ?? task.description;
+
+    await task.save();
+
+    return res.status(200).json({
+      success: true,
+      task,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+
 /* GET MY TASKS */
 export const getMyTasks = async (req, res) => {
   try {
@@ -18,6 +84,7 @@ export const getMyTasks = async (req, res) => {
     });
   }
 };
+
 
 export const createTask = async (req, res) => {
   try {
--- backend project task manager/src/routes/task.routes.js
+++ backend project task manager/src/routes/task.routes.js
@@ -3,7 +3,16 @@ import authMiddleware from "../middleware/auth.middleware.js";
 import { createTask } from "../controllers/task.controller.js";
  import { getMyTasks } from "../controllers/task.controller.js";
 
+import {
+  updateTask,
+  deleteTask,
+} from "../controllers/task.controller.js";
+
 const router = express.Router();
+
+router.put("/:id", authMiddleware, updateTask);
+router.delete("/:id", authMiddleware, deleteTask);
+
 
 router.post("/", authMiddleware, createTask);
 router.get("/", authMiddleware, getMyTasks);
